234b4e0d88457bb47e285d0b69b5a1df1ad50679
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-04-19 19:34:55 +0800

    github mcp client test passed

---
+++ src/tools/mcp_client.py
@@ -0,0 +1,43 @@
+import asyncio 
+from typing import List
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from src.core.config import settings
+from src.logger.custom_logger import logger 
+from src.exceptions.custom_exceptions import MCPToolException
+
+class GitHubMCPClient:
+    def __init__(self):
+        if not settings.GITHUB_PERSONAL_ACCESS_TOKEN:
+            raise MCPToolException("GITHUB_PERSONAL_ACCESS_TOKEN not found")
+        
+        self._connections = {
+            "github":{
+                "transport":"streamable_http",
+                "url":settings.GITHUB_MCP_URL,
+                "headers": {
+                    "Authorization": f"Bearer {settings.GITHUB_PERSONAL_ACCESS_TOKEN}"
+                },
+            }
+        }
+    
+    async def _load_tools(self) -> List:
+        try:
+            client = MultiServerMCPClient(self._connections)
+            tools = await client.get_tools()
+            logger.bind(agent="mcp").info(f"Loaded {len(tools)} GitHub MCP tools:"
+                                          f"{[t.name for t in tools]}")
+            
+            return tools 
+    
+        except Exception as e:
+            raise MCPToolException(f"Failed to load GitHub MCP tools :{e}") from e 
+    
+    def load_tools_sync(self) -> List:
+        return asyncio.run(self._load_tools())
+
+# if __name__ == "__main__":
+#     client = GitHubMCPClient()
+#     mcp_tools = client.load_tools_sync() 
+#     logger.info(f"mcp_tools:{mcp_tools}")
